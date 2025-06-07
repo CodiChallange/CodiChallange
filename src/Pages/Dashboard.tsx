377639df7947858f52fd7d372c74d609ddcd6f3b
Janderff <janderffigueiredo@gmail.com>
2025-06-07 20:01:03 -0300

    Botao de filtro criado com popover

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,8 +1,10 @@
 import { InfoCard } from '@/Components/InfoCard'
 import { Aside } from '@/Components/Aside'
 import { BanknoteArrowDown, ChartNoAxesCombined } from 'lucide-react'
-import { Button } from '@/Components/Button'
+import { Button } from '@/Components/ui/button'
 import { useNavigate } from 'react-router-dom'
+import { Popover } from '@radix-ui/react-popover'
+import { PopoverContent, PopoverTrigger } from '@/Components/ui/popover'
 
 export function Dashboard() {
   const navigate = useNavigate()
@@ -13,7 +15,7 @@ export function Dashboard() {
     <div className='flex '>
       <Aside />
       <main className=' bg-[#FFFFFF] h-screen w-full'>
-        <div className='m-8 flex justify-between items-center border'>
+        <div className='m-8 flex justify-between items-center p-4'>
           <div>
             <h1 className='text-2xl font-bold'>Dashboard Financeiro</h1>
             <p className='text-sm text-zinc-600'>
@@ -21,8 +23,19 @@ export function Dashboard() {
             </p>
           </div>
           <div className='flex gap-2 mr-8'>
+            <Popover>
+              <PopoverTrigger>
+                <Button variant='outline'>Filtros</Button>
+              </PopoverTrigger>
+              <PopoverContent className='w-65'>
+                <div className='flex flex-row justify-center '>
+                  <span className='p-2 rounded ml-3  border-2'>semana</span>
+                  <span className='p-2 rounded ml-3  border-2'>mes</span>
+                  <span className='p-2 rounded ml-3  border-2'>ano</span>
+                </div>
+              </PopoverContent>
+            </Popover>
             <button>Este Mês</button>
-            <button>Filtros</button>
           </div>
         </div>
         <section className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
@@ -45,7 +58,7 @@ export function Dashboard() {
           />
           <InfoCard icon={BanknoteArrowDown} name='Crescimento' value='15.5%' />
         </section>
-        <section className='m-8 p-2 border-2 border-purple-200 rounded-lg flex flex-col bg-purple-100 p-8 '>
+        <section className='m-8 border-2 border-purple-200 rounded-lg flex flex-col bg-purple-100 p-8 '>
           <div>
             <h3 className='text-xl font-semibold text-[#A243D2]'>
               Ações Rapidas
