f0d4b8d5c31cfcb582dffc1dbf6d8a32eb4ae915
EduardoHill <eduardohillfav@gmail.com>
2025-06-17 08:24:17 -0300

    feat: Criando Tabs Area

--- src/Pages/Relatorios.tsx
+++ src/Pages/Relatorios.tsx
@@ -1,5 +1,7 @@
 import { Aside } from '@/Components/Aside'
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/Components/ui/tabs'
+
 export function Relatorios() {
   return (
     <div className='flex h-screen'>
@@ -7,9 +9,16 @@ export function Relatorios() {
       <div className='flex flex-col w-full p-5 bg-gray-100 gap-8 overflow-auto'>
         <div>Header- Eduardo</div>
         <div>Cards- Jander</div>
-        <div>Toggle -Eduardo</div>
-        <div>Grasficos -Jander</div>
-        <div>Detalhamento -Eduardo</div>
+        <Tabs defaultValue='account' className='w-[400px] text-black'>
+          <TabsList>
+            <TabsTrigger value='account'>Account</TabsTrigger>
+            <TabsTrigger value='password'>Password</TabsTrigger>
+          </TabsList>
+          <TabsContent value='account'>
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value='password'>Change your password here.</TabsContent>
+        </Tabs>
       </div>
     </div>
   )
