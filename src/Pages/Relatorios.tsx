835055c14baf7ef65495aa9e9721dc840653ac2b
EduardoHill <eduardohillfav@gmail.com>
2025-06-17 08:19:25 -0300

    style: Arrumando style base da pagina

--- src/Pages/Relatorios.tsx
+++ src/Pages/Relatorios.tsx
@@ -4,7 +4,7 @@ export function Relatorios() {
   return (
     <div className='flex h-screen'>
       <Aside />
-      <div className='overflow-auto'>
+      <div className='flex flex-col w-full p-5 bg-gray-100 gap-8 overflow-auto'>
         <div>Header- Eduardo</div>
         <div>Cards- Jander</div>
         <div>Toggle -Eduardo</div>
