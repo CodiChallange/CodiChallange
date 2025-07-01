ebacf4a61c50d5450790639b424d98cd36ec09b1
Eduardo Hill <eduardohillfav@gmail.com>
2025-07-01 17:09:34 -0300

    fix: arrumando bug do UserCard

--- src/Components/UsersCard.tsx
+++ src/Components/UsersCard.tsx
@@ -1,18 +1,18 @@
-import { Calendar, Mail, Phone, SquarePen, Trash2 } from 'lucide-react'
-import Logo from '../assests/CodiLogoAside.png'
+import { Calendar, Mail, Phone, SquarePen, Trash2 } from "lucide-react";
+import Logo from "../assests/CodiLogoAside.png";
 
 type cardProps = {
-  name: string
-  key: string
-  email: string
-  departament: string
-  phone: string
-  status: string
-  salario: number
-  cargo: string
-  onEdit: () => void
-  onRemove: () => void
-}
+  name: string;
+  key: string;
+  email: string;
+  departament: string;
+  phone: string;
+  status: string;
+  salario: number;
+  cargo: string;
+  onEdit: () => void;
+  onRemove: () => void;
+};
 
 export function UserCard({
   name,
@@ -26,24 +26,24 @@ export function UserCard({
   onRemove,
 }: cardProps) {
   return (
-    <div className='flex flex-col bg-white md:w-full 2xl:w-[470px]  p-3 gap-3 border rounded-lg hover:shadow-2xl transition-shadow duration-500 ease-in-out'>
+    <div className="flex flex-col gap-3 rounded-lg border bg-white p-3 transition-shadow duration-500 ease-in-out hover:shadow-2xl md:w-full lg:w-[415px]">
       {/* Header */}
-      <div className='flex items-center justify-between'>
-        <div className='flex items-center gap-1 '>
-          <img src={Logo} alt='' className='w-10 h-10' />
-          <div className='flex flex-col'>
-            <h1 className='font-semibold'>{name}</h1>
-            <p className='text-gray-500'>{cargo}</p>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <img src={Logo} alt="" className="h-10 w-10" />
+          <div className="flex flex-col">
+            <h1 className="font-semibold">{name}</h1>
+            <p className="text-gray-500">{cargo}</p>
           </div>
         </div>
         <div
-          className={` px-2 py-0.5 rounded-2xl flex items-center justify-center ${
-            status === 'Ativo' ? 'bg-green-100' : 'bg-yellow-100'
+          className={`flex items-center justify-center rounded-2xl px-2 py-0.5 ${
+            status === "Ativo" ? "bg-green-100" : "bg-yellow-100"
           }`}
         >
           <span
             className={`text-[14px] ${
-              status === 'Ativo' ? ' text-green-800' : ' text-yellow-800'
+              status === "Ativo" ? "text-green-800" : "text-yellow-800"
             }`}
           >
             {status}
@@ -52,52 +52,52 @@ export function UserCard({
       </div>
 
       {/* Content */}
-      <div className='flex flex-col gap-1 border-b pb-5'>
-        <span className='flex text-gray-700 text-[14px] items-center gap-2 '>
+      <div className="flex flex-col gap-1 border-b pb-5">
+        <span className="flex items-center gap-2 text-[14px] text-gray-700">
           <Mail size={16} /> {email}
         </span>
-        <span className='flex text-gray-700 text-[14px] items-center gap-2 '>
+        <span className="flex items-center gap-2 text-[14px] text-gray-700">
           <Phone size={16} /> {phone}
         </span>
-        <span className='flex text-gray-700 text-[14px] items-center gap-2 '>
+        <span className="flex items-center gap-2 text-[14px] text-gray-700">
           <Calendar size={16} /> Desde 14/01/2023
         </span>
       </div>
 
       {/* Data */}
-      <div className='flex flex-col gap-2'>
-        <div className='flex justify-between items-center'>
-          <span className='text-gray-900 text-[15px] font-semibold '>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <span className="text-[15px] font-semibold text-gray-900">
             Departamento:
           </span>
-          <span className='text-gray-600 text-[14px] '>{departament}</span>
+          <span className="text-[14px] text-gray-600">{departament}</span>
         </div>
-        <div className='flex justify-between items-center'>
-          <span className='text-gray-900 text-[15px] font-semibold '>
+        <div className="flex items-center justify-between">
+          <span className="text-[15px] font-semibold text-gray-900">
             Salário:
           </span>
-          <span className='text-[#A243D2] text-[14px] font-bold'>
+          <span className="text-[14px] font-bold text-[#A243D2]">
             R$ {salario}
           </span>
         </div>
       </div>
 
       {/* Button area */}
-      <div className='flex gap-2'>
+      <div className="flex gap-2">
         <button
           onClick={onEdit}
-          className='flex border rounded-lg p-2 w-11/12 items-center justify-center gap-3 font-bold cursor-pointer hover:bg-gray-100'
+          className="flex w-11/12 cursor-pointer items-center justify-center gap-3 rounded-lg border p-2 font-bold hover:bg-gray-100"
         >
           <SquarePen />
           Editar
         </button>
         <button
           onClick={onRemove}
-          className='flex border rounded-lg p-2 font-bold cursor-pointer hover:bg-gray-100 items-center justify-center text-red-500'
+          className="flex cursor-pointer items-center justify-center rounded-lg border p-2 font-bold text-red-500 hover:bg-gray-100"
         >
           <Trash2 />
         </button>
       </div>
     </div>
-  )
+  );
 }
