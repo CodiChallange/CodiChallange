f9fe0ccfdfc13864f820065512dac36f20aedabb
EduardoHill <eduardohillfav@gmail.com>
2025-07-01 20:27:32 -0300

    feat: Criando estrutura base dos cards da CodiStore

--- src/Components/CardStore.tsx
+++ src/Components/CardStore.tsx
@@ -1,7 +1,43 @@
 export function CardStore() {
   return (
     <div>
-      <h1>Card</h1>
+      {/* Header */}
+      <header>
+        <h1>Venda #0000</h1>
+      </header>
+
+      {/* Info Area */}
+      <div>
+        <h2>Bruno</h2>
+        <h2>
+          <span>Turno: </span>Manhã
+        </h2>
+      </div>
+
+      {/* Content */}
+      <main>
+        <h1>Itens:</h1>
+        <ul>
+          <li>
+            <span>1x</span> Coca-Cola
+          </li>
+          <li>
+            <span>1x</span> Hamburguer
+          </li>
+          <li>
+            <span>1x</span> Suco
+          </li>
+        </ul>
+      </main>
+
+      {/* Valor e Botão */}
+      <footer>
+        <div>
+          <h1>Valor:</h1>
+          <span>R$ 58,56</span>
+        </div>
+        <button>Detalhes</button>
+      </footer>
     </div>
   );
 }
